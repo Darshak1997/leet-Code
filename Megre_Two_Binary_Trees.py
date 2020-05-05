803c5a5c964b4f1d0017676a35e8f333382568f7
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-05 18:09:16 -0500

    Update Megre_Two_Binary_Trees.py

--- Megre_Two_Binary_Trees.py
+++ Megre_Two_Binary_Trees.py
@@ -6,6 +6,24 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        ### BFS ###
+        if (not t1 and t2):
+            return None
+        queue1, queue2 = collections.deque([t1]), collections.deque([t2])
+        while queue1 and queue2:
+            node1, node2 = queue1.popleft(), queue2.popleft()
+            if node1 and node2:
+                node1.val = node1.val + node2.val
+                if (not node1.left) and node2.left:
+                    node1.left = TreeNode(0)
+                if (not node1.right) and node2.right:
+                    node1.right = TreeNode(0)
+                queue1.append(node1.left)
+                queue1.append(node1.right)
+                queue2.append(node2.left)
+                queue2.append(node2.right)
+        return t1
+        ### Recurssion ###
         if not t1 and not t2:
             return None
         ans = TreeNode((t1.val if t1 else 0) + (t2.val if t2 else 0))
