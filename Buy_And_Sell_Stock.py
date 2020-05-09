ac88674103e27df725d8a313337ff147eaa29cb8
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-09 00:07:14 -0500

    Update Buy_And_Sell_Stock.py

--- Buy_And_Sell_Stock.py
+++ Buy_And_Sell_Stock.py
@@ -1,14 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 0:
+        if prices == []:
             return 0
-        low = float("inf")
-        profit = 0
-        for price in prices:
-            if price > low:
-                if price - low > profit:
-                    profit = price - low
-            elif price < low:
-                low = price
+        buy = prices[0] 
+        res = 0
+        for i in range(1, len(prices)):
+            diff = prices[i]-buy
+            if diff > res:
+                res = diff
+            if buy > prices[i]:
+                buy = prices[i]
+        return res
+    
+#         if len(prices) == 0:
+#             return 0
+#         low = float("inf")
+#         profit = 0
+#         for price in prices:
+#             if price > low:
+#                 if price - low > profit:
+#                     profit = price - low
+#             elif price < low:
+#                 low = price
                     
-        return profit
+#         return profit
