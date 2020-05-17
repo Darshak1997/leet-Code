9b4716b72fc3d6e8118e68abf514f7441466c3ca
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-17 17:58:02 -0500

    Update Paint_House.py

--- Paint_House.py
+++ Paint_House.py
@@ -1,21 +1,10 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        def paint_cost(n, color):
-            if (n,color) in self.memo:
-                return self.memo[(n, color)]
-            total = costs[n][color]
-            if n == len(costs) - 1:
-                pass
-            elif color == 0:
-                total += min(paint_cost(n+1, 1), paint_cost(n+1, 2))
-            elif color == 1:
-                total += min(paint_cost(n+1, 0), paint_cost(n+1, 2))
-            elif color == 2:
-                total += min(paint_cost(n+1, 0), paint_cost(n+1, 1))
-            self.memo[(n, color)] = total
-            return total
-        
-        if costs == []:
+        if(costs == None or len(costs) == 0):
             return 0
-        self.memo = {}
-        return min(paint_cost(0,0), paint_cost(0,1), paint_cost(0,2))
+        for i in range(1, len(costs)):
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2])
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2])
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1])
+        
+        return min(min(costs[len(costs)-1][0], costs[len(costs)-1][1]), costs[len(costs)-1][2])
