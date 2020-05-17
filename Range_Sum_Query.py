a2148629c296c5d788f849114362cf291edcf721
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-17 14:30:12 -0500

    Create Range_Sum_Query.py

---
+++ Range_Sum_Query.py
@@ -0,0 +1,17 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        # self.data = nums
+        self.dp = nums
+        for i in range(1, len(nums)):
+            self.dp[i] += self.dp[i-1]
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.dp[j] - (self.dp[i-1] if i > 0 else 0)
+        # total = sum(self.data[i:j+1])
+        # return total
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
