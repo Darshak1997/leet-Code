e81edcaed931d315ba9588d8856535bbd7fa4f2d
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-22 23:08:51 -0500

    Create MergeTwoLists.py

---
+++ MergeTwoLists.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, a, b):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not a or b and a.val > b.val:
+            a, b = b, a
+        if a:
+            a.next = self.mergeTwoLists(a.next, b)
+        return a
