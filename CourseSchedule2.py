7880a154e0fe857f77430b47457010e17144fcc6
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-05-30 20:21:55 -0500

    Create CourseSchedule2.py

---
+++ CourseSchedule2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dic = collections.defaultdict(set)
+        neigh = collections.defaultdict(set)
+        for i, j in prerequisites:
+            dic[i].add(j)
+            neigh[j].add(i)
+        stack = [i for i in range(numCourses) if not dic[i]]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            for i in neigh[node]:
+                dic[i].remove(node)
+                if not dic[i]:
+                    stack.append(i)
+            dic.pop(node)
+        return res if not dic else []
+                
