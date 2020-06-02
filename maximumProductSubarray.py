e5451fab0ab154c07945c515735e2efaa530cbb3
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-06-02 17:27:47 -0500

    Create maximumProductSubarray.py

---
+++ maximumProductSubarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        B = nums[::-1]
+        for i in range(1, len(nums)):
+            nums[i] *= nums[i-1] or 1
+            B[i] *= B[i-1] or 1
+            # print(nums)
+        return max(nums+B)
