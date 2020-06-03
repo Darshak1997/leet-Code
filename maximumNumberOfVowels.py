976612b7ecd4603621e6b920b1e042ddcbdf0464
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-06-03 13:41:48 -0500

    Create maximumNumberOfVowels.py

---
+++ maximumNumberOfVowels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        res = j = vowels = 0
+        for i, c in enumerate(s):
+            vowels += c in 'aeiou' 
+            if i - j + 1 > k:
+                vowels -= s[j] in 'aeiou'
+                j += 1    
+            if i - j + 1 == k:    
+                res = max(res, vowels)
+                # print(s[i:j+1], res)
+        return res 
+    
