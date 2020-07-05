5f10280349ff9ea1782eaf1cd7b1394fb068f27d
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-07-05 17:04:19 -0500

    Create validPerfectSquare.py

---
+++ validPerfectSquare.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = num
+        while left <= right:
+            mid = (left + right)//2
+            if mid ** 2 == num:
+                return True
+            elif mid ** 2 > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
