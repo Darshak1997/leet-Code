59f3d1ed793cb651739dacc6343a5736b8f716be
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-08-02 13:00:54 -0500

    Update four_sum.py

--- four_sum.py
+++ four_sum.py
@@ -2,15 +2,28 @@ def search_quadruplets(arr, target):
   arr.sort()
   quadruplets = []
   # TODO: Write your code here
-  for i in range(len(arr)-3):
+  for i in range(0, len(arr)-3):
+    # To avoid duplicates
+    if i > 0 and arr[i] == arr[i+1]:
+      continue
     for j in range(i+1, len(arr)-2):
+      # To avoid duplicates
+      if j > i+1 and arr[j] == arr[j-1]:
+        continue
       left = j+1
       right = len(arr)-1
       while left < right:
         currentSum = arr[i] + arr[j] + arr[left] + arr[right]
         if currentSum == target:
           quadruplets.append([arr[i], arr[j], arr[left], arr[right]])
-          break
+          left += 1
+          right -= 1
+          # To avoid duplicates
+          while (left < right and arr[left] == arr[left-1]):
+            left += 1
+          # TO avoid duplicates
+          while (left < right and arr[right] == arr[right+1]):
+            right -= 1
         elif currentSum < target:
           left += 1
         else:
