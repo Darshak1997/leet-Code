17cef52d2f7a27e53c46047c201470e32c2d544f
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-09-30 12:51:07 -0500

    Update maximumProductSubarray.py

--- maximumProductSubarray.py
+++ maximumProductSubarray.py
@@ -1,8 +1,31 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        B = nums[::-1]
-        for i in range(1, len(nums)):
-            nums[i] *= nums[i-1] or 1
-            B[i] *= B[i-1] or 1
-            # print(nums)
-        return max(nums+B)
+        res = -float('inf')
+        product = 1
+        length = len(nums)
+        for i in range(length):
+            product *= nums[i]
+            res = max(product, res)
+            if nums[i] == 0:
+                product = 1
+        # scan backward
+        product = 1
+        for j in range(length - 1, -1, -1):
+            product *= nums[j]
+            res = max(product, res)
+            if nums[j] == 0:
+                product = 1
+        return res
+        # maxArr = []
+        # for start in range(len(nums)):
+        #     currMax = -inf
+        #     end = start + 1
+        #     currProduct = nums[start]
+        #     # currMax = max(currMax, currProduct)
+        #     currMax = currProduct
+        #     while end <= len(nums)-1:
+        #         currMax = max(currMax, currProduct)
+        #         currProduct *= nums[end]
+        #         end += 1
+        #     maxArr.append(currMax)
+        # return max(maxArr)
