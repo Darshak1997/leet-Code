61d700df54eaac0794a345cfba66dafaa92ed5e5
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-10-06 17:41:04 -0500

    Create Integer_To_Roman.py

---
+++ Amazon Interview/Arrays and Strings/Integer_To_Roman.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        mapping = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        result = ""
+        
+        for k in mapping:
+            
+            while num >= k:
+                result += mapping[k]
+                num -= k
+            
+        return result
