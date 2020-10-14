0bcf5afad7f0a2d48be755c13db6cbce421e4ef0
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-10-14 17:16:24 -0500

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -1,52 +1,34 @@
-class Node:
-    def __init__(self, k, v):
-        self.key = k
-        self.val = v
-        self.prev = None
-        self.next = None
-    
-class LRUCache:
+from collections import OrderedDict
+class LRUCache(OrderedDict):
 
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
         self.capacity = capacity
-        self.dic = dict()
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
 
-    def get(self, key: int) -> int:
-        # print("Get Function: ", self.queue[0].keys())
-        if key in self.dic:
-            n = self.dic[key]
-            self._remove(n)
-            self._add(n)
-            return n.val
-        return -1
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self:
+            return - 1
+        
+        self.move_to_end(key)
+        return self[key]
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self._remove(self.dic[key])
-        n = Node(key, value)
-        self._add(n)
-        self.dic[key] = n
-        if len(self.dic) > self.capacity:
-            n = self.head.next
-            self._remove(n)
-            del self.dic[n.key]
-    
-    def _remove(self, node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
-
-    def _add(self, node):
-        p = self.tail.prev
-        p.next = node
-        self.tail.prev = node
-        node.prev = p
-        node.next = self.tail
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+        if len(self) > self.capacity:
+            self.popitem(last = False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
