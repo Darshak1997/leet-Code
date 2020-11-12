14f7b583dbb65c88f3328c38394aa3b0a0cb8709
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2020-11-12 01:29:15 -0600

    Update Walls_And_Gates.py

--- Walls_And_Gates.py
+++ Walls_And_Gates.py
@@ -3,23 +3,22 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        if not rooms or not rooms[0]:
+        max_value = 2147483647
+        if not rooms:
             return
-        row, col, inf = len(rooms), len(rooms[0]), 2147483647
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        queue = []
-        for i in range(row):
-            for j in range(col):
+        q = deque()
+        
+        for i in range(len(rooms)):
+            for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
-                    queue.append((i, j))
-        step = 0
-        while queue:
-            new_queue = []
-            step += 1
-            for (x, y) in queue:
-                for d in directions:
-                    nx, ny = x+d[0], y+d[1]
-                    if 0<=nx<row and 0<=ny<col and rooms[nx][ny] == inf:
-                        rooms[nx][ny] = step
-                        new_queue.append((nx, ny))
-            queue = new_queue
+                    q.append((i,j))
+        
+        while q:
+            row, col = q.popleft()
+            dist = rooms[row][col]+1
+            for dx, dy in (-1,0),(0,1),(0,-1),(1,0):
+                r = dy+row
+                c = dx+col
+                if (r >= 0 and r < len(rooms)) and (c >= 0 and c < len(rooms[0])) and (rooms[r][c] == max_value):
+                    rooms[r][c] = dist
+                    q.append((r,c))
